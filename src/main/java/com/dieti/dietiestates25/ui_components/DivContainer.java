745edba937837cd58b2efbc1389fca23a33464fb
sansarc <sansevieroarcangelo@gmail.com>
2025-02-27 21:23:24 -0500

    added it to listener and implemented custom dark mode.

--- src/main/java/com/dieti/dietiestates25/ui_components/DivContainer.java
+++ src/main/java/com/dieti/dietiestates25/ui_components/DivContainer.java
@@ -1,23 +1,45 @@
 package com.dieti.dietiestates25.ui_components;
 
+import com.dieti.dietiestates25.constants.Constants;
+import com.dieti.dietiestates25.observers.ThemeChangeListener;
+import com.dieti.dietiestates25.observers.ThemeChangeNotifier;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.server.VaadinSession;
 import com.vaadin.flow.theme.lumo.LumoUtility;
 
-public class DivContainer extends Div {
+public class DivContainer extends Div implements ThemeChangeListener {
 
     public DivContainer(String width, String height) {
+        ThemeChangeNotifier.addListener(this);
+        addDetachListener(event -> ThemeChangeNotifier.removeListener(this));
+        Boolean darkTheme = (Boolean) VaadinSession.getCurrent().getAttribute("darkTheme");
+        applyTheme(darkTheme != null ? darkTheme : false);
+
+        configureLayout(width, height);
+    }
+
+    private void configureLayout(String width, String height) {
         addClassNames(
                 LumoUtility.Background.BASE,
                 LumoUtility.BoxShadow.MEDIUM,
                 LumoUtility.BorderRadius.LARGE,
-                LumoUtility.Padding.LARGE
+                LumoUtility.Padding.LARGE,
+                LumoUtility.Gap.XSMALL,
+                LumoUtility.Display.FLEX,
+                LumoUtility.FlexDirection.COLUMN
         );
 
         setWidth(width);
         setHeight(height);
+    }
 
-        getStyle().set("display", "flex")
-                .set("flex-direction", "column")
-                .set("gap", "var(--lumo-space-xs)");
+    @Override
+    public void onThemeChange(boolean darkTheme) {
+        applyTheme(darkTheme);
+    }
+
+    private void applyTheme(boolean darkTheme) {
+        if (darkTheme)
+            getStyle().setBackgroundColor(Constants.Colors.GRAY_OVER_DARKMODE);
     }
 }
