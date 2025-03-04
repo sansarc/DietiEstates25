f7f40387010949bc528d5807272cf4a3a6b7517f
sansarc <sansevieroarcangelo@gmail.com>
2025-03-04 15:01:23 -0500

    Ad presentation draft idea cont.

--- src/main/java/com/dieti/dietiestates25/views/AdView.java
+++ src/main/java/com/dieti/dietiestates25/views/AdView.java
@@ -1,7 +1,7 @@
 package com.dieti.dietiestates25.views;
 
 import com.dieti.dietiestates25.ui_components.DivContainer;
-import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.avatar.Avatar;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.html.Image;
@@ -10,16 +10,16 @@ import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.Scroller;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.dom.Style;
-import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
 @Route(value = "ad", layout = MainLayout.class)
 public class AdView extends VerticalLayout {
 
+    public static final String SCROLLER_CONTENT_WIDTH = "90%";
     HorizontalLayout picturesLayout = new HorizontalLayout();
-    VerticalLayout leftLayout = new VerticalLayout();
+    VerticalLayout scrollerContent = new VerticalLayout();
     HorizontalLayout layout = new HorizontalLayout();
-    Scroller leftScroller = new Scroller(leftLayout);
+    Scroller scroller = new Scroller(scrollerContent);
 
     public AdView() {
         configureLayout();
@@ -35,23 +35,26 @@ public class AdView extends VerticalLayout {
         var rightButton = new Button(VaadinIcon.ARROW_RIGHT.create());
         picturesLayout.add(leftButton, photos, rightButton);
 
-        var generalInfoDiv = new DivContainer("90%", "200px");
-        var descriptionDiv = new DivContainer("90%", "400px");
+        var generalInfoDiv = new DivContainer(SCROLLER_CONTENT_WIDTH, "200px");
+        var descriptionDiv = new DivContainer(SCROLLER_CONTENT_WIDTH, "400px");
         var descriptionTitle = new H3("About this home");
         descriptionDiv.add(descriptionTitle);
-        var mapDiv = new DivContainer("90%", "900px");
+        var mapDiv = new DivContainer(SCROLLER_CONTENT_WIDTH, "900px");
         var mapTitle = new H3("What's around this home");
         mapDiv.add(mapTitle);
 
-        var bidDiv = new DivContainer("40%", "400px");
+        var bidDiv = new DivContainer("30%", "400px");
         bidDiv.getStyle()
                 .setPosition(Style.Position.ABSOLUTE)
                 .setRight("0")
                 .setTop("0")
                 .setTransition("transform 0.1s");
+        var bidTitle = new H3("About the agent");
+        var agentAvatar = new Avatar();
+        bidDiv.add(bidTitle);
 
-        leftLayout.add(generalInfoDiv, descriptionDiv, mapDiv);
-        layout.add(leftScroller, bidDiv);
+        scrollerContent.add(generalInfoDiv, descriptionDiv, mapDiv);
+        layout.add(scroller, bidDiv);
         layout.getStyle().setPosition(Style.Position.RELATIVE);
 
         add(picturesLayout, layout);
@@ -68,9 +71,11 @@ public class AdView extends VerticalLayout {
         picturesLayout.setJustifyContentMode(JustifyContentMode.CENTER);
         picturesLayout.setAlignItems(Alignment.CENTER);
 
-        leftLayout.setSizeFull();
-        layout.setSizeFull();
+        scroller.setHeightFull();
+        scroller.setWidth("70%");
+        scrollerContent.setWidthFull();
 
-        leftScroller.setSizeFull();
+        layout.setWidth("70%");
+        layout.setHeightFull();
     }
 }
