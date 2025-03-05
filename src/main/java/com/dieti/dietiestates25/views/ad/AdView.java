c634650ac5fe9d9b41b1c8f9a7c09594ad148413
sansarc <sansevieroarcangelo@gmail.com>
2025-03-05 13:54:17 -0500

    added Map component and implementation.

--- src/main/java/com/dieti/dietiestates25/views/ad/AdView.java
+++ src/main/java/com/dieti/dietiestates25/views/ad/AdView.java
@@ -1,31 +1,22 @@
-package com.dieti.dietiestates25.views;
+package com.dieti.dietiestates25.views.ad;
 
-import com.dieti.dietiestates25.constants.Constants;
-import com.dieti.dietiestates25.services.geoapify.GeoapifyService;
 import com.dieti.dietiestates25.ui_components.DivContainer;
-import software.xdev.vaadin.maps.leaflet.map.*;
+import com.dieti.dietiestates25.ui_components.Map;
+import com.dieti.dietiestates25.views.MainLayout;
 import com.vaadin.flow.component.avatar.Avatar;
-import com.vaadin.flow.component.avatar.AvatarVariant;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
-import com.vaadin.flow.component.html.*;
+import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.Scroller;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.NumberField;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.dom.Style;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.theme.lumo.LumoUtility;
-import org.springframework.beans.factory.annotation.Autowired;
+import software.xdev.vaadin.maps.leaflet.registry.LDefaultComponentManagementRegistry;
 
 @Route(value = "ad", layout = MainLayout.class)
 public class AdView extends VerticalLayout {
-
-    @Autowired
-    private GeoapifyService geoapifyService;
 
     public static final String SCROLLER_CONTENT_WIDTH = "90%";
     HorizontalLayout picturesLayout = new HorizontalLayout();
@@ -36,6 +27,7 @@ public class AdView extends VerticalLayout {
     public AdView() {
         configureLayout();
         configureComponents();
+        // addScrollTracking();
     }
 
     private void configureComponents() {
@@ -50,41 +42,22 @@ public class AdView extends VerticalLayout {
         var descriptionDiv = new DivContainer(SCROLLER_CONTENT_WIDTH, "400px");
         var descriptionTitle = new H3("About this home");
         descriptionDiv.add(descriptionTitle);
-        var mapDiv = new DivContainer(SCROLLER_CONTENT_WIDTH, "900px");
+        var mapDiv = new DivContainer(SCROLLER_CONTENT_WIDTH, "700px");
         var mapTitle = new H3("What's around this home");
-        var map = new Div();
-        mapDiv.add(mapTitle, map);
+        final var registry = new LDefaultComponentManagementRegistry(this);
+        final var map = new Map(registry, 52.5200, 13.4050, 12); // Berlin
 
+        map.addMarker(registry, 52.5200, 13.4050, "Berlin");
+        mapDiv.add(mapTitle, map);
         var bidDiv = new DivContainer("30%", "400px");
         bidDiv.getStyle()
                 .setPosition(Style.Position.ABSOLUTE)
                 .setRight("0")
                 .setTop("0")
                 .setTransition("transform 0.1s");
-        var bidTitle = new H3("Set your price");
-        bidTitle.getStyle().setPaddingBottom("20px");
-        var agentAvatar = new Avatar("John Doe");
-        agentAvatar.addThemeVariants(AvatarVariant.LUMO_XLARGE);
-        agentAvatar.getStyle()
-                .setCursor("pointer")
-                .setColor(Constants.Colors.PRIMARY_BLUE)
-                .setFontWeight(Style.FontWeight.BOLDER)
-                .setBorder("1px solid " + Constants.Colors.PRIMARY_BLUE);
-        agentAvatar.getAbbreviation();
-        var agentName = new H4("John Doe");
-        agentName.getStyle().setCursor("pointer");
-        var agentProfileLayout = new HorizontalLayout(agentAvatar, agentName);
-        agentProfileLayout.setAlignItems(Alignment.CENTER);
-        agentProfileLayout.getStyle().setPaddingBottom("30px");
-        var bidTextField = new NumberField("Your offer", "");
-        bidTextField.setSuffixComponent(VaadinIcon.EURO.create());
-        bidTextField.getStyle()
-                .set("--vaadin-input-field-height", "30px")
-                .set("--vaadin-input-field-value-font-size", "20px")
-                .set("--vaadin-input-field-value-font-weight", LumoUtility.FontWeight.BOLD);
-        var confirmBid = new Button("Confirm");
-        confirmBid.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-        bidDiv.add(bidTitle, agentProfileLayout, bidTextField, confirmBid);
+        var bidTitle = new H3("About the agent");
+        var agentAvatar = new Avatar();
+        bidDiv.add(bidTitle);
 
         scrollerContent.add(generalInfoDiv, descriptionDiv, mapDiv);
         layout.add(scroller, bidDiv);
@@ -105,10 +78,10 @@ public class AdView extends VerticalLayout {
         picturesLayout.setAlignItems(Alignment.CENTER);
 
         scroller.setHeightFull();
-        scroller.setWidth("65%");
+        scroller.setWidth("70%");
         scrollerContent.setWidthFull();
 
-        layout.setWidth("90%");
+        layout.setWidth("70%");
         layout.setHeightFull();
     }
 }
